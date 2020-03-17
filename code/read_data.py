b5c10ba91904b7eb5ce9200c8c52a9dbe50f088b
Laurence <39910035+LaurenceBont@users.noreply.github.com>
2020-03-17 16:48:04 +0100

    Added some comments to the code
    Given a file for one person, this file can be converted to a tensor (x_chrom, n_embed, n_sequence_length). To do, make this callable by the dataloader. Will upload the dataloader later.

--- code/read_data.py
+++ code/read_data.py
@@ -7,6 +7,9 @@ look_up = {"A": 0, "G": 1, "T": 2, "C": 3}
 
 
 def DNA_to_onehot(sequence):
+    """
+        Creates a matrix of size (4, max_seq_length) with only 0 vectors then fills it up accordingly.
+    """
     max_seq_length = 142230 + 1
     data = np.zeros((4,max_seq_length))
     # print(data.shape)
@@ -15,13 +18,19 @@ def DNA_to_onehot(sequence):
     return torch.from_numpy(data).float()
 
 def process_line(line, filepath):
+    """
+        Processes a given line to aquire the sequence and the name of the chromosome
+    """
     pieces = line.split(",")
     seq = pieces[1]
     chrom = pieces[0].split(":")[2]
     return seq, chrom
 
 
-def importData(filepath):
+def importData(filepath, to_write = True):
+    """
+        Imports the data and prints if needed. Also provides a warning if we miss a label.
+    """
     f = open(filepath)
     genes = {}
     label = None
@@ -35,12 +44,14 @@ def importData(filepath):
             label = 0
         seq, chrom = process_line(line, filepath)
 
-        text_file = open("lengths_sequences.txt", "a")
-        n = text_file.write(f"File: {filepath} \n")
-        n = text_file.write(f"Current chrom: {chrom} with sequence length: {str(len(seq))} \n")
-        n = text_file.write(f"--------")
+        if to_write:
+            text_file = open("lengths_sequences.txt", "a")
+            n = text_file.write(f"File: {filepath} \n")
+            n = text_file.write(f"Current chrom: {chrom} with sequence length: {str(len(seq))} \n")
+            n = text_file.write(f"--------")
         genes[chrom] = DNA_to_onehot(seq)
-    text_file.close()
+    if to_write:
+        text_file.close()
     return genes, label
 
 def print_shapes(genes):
@@ -50,6 +61,9 @@ def print_shapes(genes):
     print("-----")
 
 def obtain_all_files(filepath):
+    """
+        Returns all files for a specific folder that end in .txt
+    """
     file_list = []
     for filename in os.listdir(filepath):
         if filename.endswith(".txt"): 
@@ -60,12 +74,16 @@ def obtain_all_files(filepath):
     return file_list
 
 def set_up_tensors(genes):
+    """
+        Given a dict of genes, stack them and return them in one big tensor (x_genes, n_embedding, n_sequence)
+    """
     chroms = ['chr5','chr8', 'chr5','chr10', 'chr13','chr5', 'chr19']
-    data_tensor = genes['chr1'] #removed chr1 from the whole set of chrs
-    for chr in chroms:
-        print("shape of current chromosome", chr.shape)
-        print(data_tensor.shape)
-        data_tensor = torch.cat((data_tensor, genes[chr]), 0)
+    data_tensor = genes['chr1'].unsqueeze(dim=0) #removed chr1 from the whole set of chrs
+    for chrm in chroms:
+        #print("shape of current data_tensor: ", data_tensor.shape)
+        #print("shape of current chromosome", genes[chrm].shape)
+        data_tensor = torch.cat(([data_tensor, genes[chrm].unsqueeze(dim=0)]), dim = 0)
+        print("shape of current all data_tensor: ", data_tensor.shape) # should be [gene, emb, n_bp] = [9,4,142231]
     return data_tensor
 
 def set_up_data(filepath):
@@ -76,8 +94,8 @@ def set_up_data(filepath):
     all_files = obtain_all_files(filepath)
     counter = 0
     for file in all_files:
-        genes, label = importData(file)
-        #data_tensor = set_up_tensors(genes)
+        genes, label = importData(file, to_write = False)
+        data_tensor = set_up_tensors(genes)
         #print("shape of tensor per file: ", data_tensor.shape)
         #print_shapes(genes)
         #print(len(genes.items()))
