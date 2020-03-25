849b3936496e0d53df967ed803875323fbb4d524
IlzeAmandaA <ilze.amanda.auzina@gmail.com>
2020-03-25 19:56:24 +0100

    original version

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import argparse
 import time
 
 #record changes
-timestr = time.strftime("%Y%m%d-%H%M%S")
-log_file = open('log/log' +timestr+'.txt', 'w')
+# timestr = time.strftime("%Y%m%d-%H%M%S")
+# log_file = open('log/log' +timestr+'.txt', 'w')
 output = 'output/'
 
 #defines arguments that can be passed to the model
@@ -33,12 +33,15 @@ args.cuda = not args.no_cuda and torch.cuda.is_available()
 
 if args.cuda:
     torch.cuda.manual_seed_all(args.seed)
-    log_file.write('GPU is working \n')
+    print('GPU is working \n')
+    # log_file.write('GPU is working \n')
 
-log_file.write('Loading the data \n')
+print('Loading the data \n')
+# log_file.write('Loading the data \n')
 DNA_dataset = Dataset(path='data_simulation', n_examples=args.dataN, print_boo=False) #max n_examples for sim_DAta 160
 
-log_file.write('Initialize Model \n')
+print('Initialize Model \n')
+# log_file.write('Initialize Model \n')
 model = AttentionNetwork()
 if args.cuda:
     model.cuda()
@@ -52,7 +55,8 @@ def train():
     #device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     loss_overall = []
     for epoch in range(1,args.epochs+1):
-        log_file.write('Epoch {}/{} \n'.format(epoch,args.epochs))
+        print('Epoch {}/{} \n'.format(epoch,args.epochs))
+        # log_file.write('Epoch {}/{} \n'.format(epoch,args.epochs))
         train_loss = 0.
         train_error = 0.
         model.train()
@@ -88,7 +92,8 @@ def train():
         train_loss /= len(idx_train)  # number of genes
         loss_overall.append(train_loss)
         train_error /= len(idx_train)
-        log_file.write('Epoch: {}, Loss: {:.4f}, train error: {:.4f} \n'.format(epoch, train_loss, train_error))
+        print('Epoch: {}, Loss: {:.4f}, train error: {:.4f} \n'.format(epoch, train_loss, train_error))
+        # log_file.write('Epoch: {}, Loss: {:.4f}, train error: {:.4f} \n'.format(epoch, train_loss, train_error))
         if epoch%10==0:
             torch.save(model.state_dict(), output + 'model_epoch_' + str(epoch) + '_.pth')
             pkl.dump(loss_overall, open(output+'loss_train.pkl','wb'))
@@ -123,13 +128,16 @@ def test(idx_validation, epoch):
         bag_level = (y.cpu().data.numpy()[0], int(predicted_label.cpu().data.numpy()[0][0]))
         instance_level = list(np.round(attention_weights.cpu().data.numpy()[0], decimals=3))
 
-        log_file.write('\nTrue Bag Label, Predicted Bag Label: {}\n'
+        print('\nTrue Bag Label, Predicted Bag Label: {}\n'
                   'Attention Weights: {} \n'.format(bag_level, instance_level))
+        # log_file.write('\nTrue Bag Label, Predicted Bag Label: {}\n'
+        #           'Attention Weights: {} \n'.format(bag_level, instance_level))
 
     test_error /= len(idx_validation)
     test_loss /= len(idx_validation)    # * x.shape[0]
 
-    log_file.write('\nTest Set, Loss: {:.4f}, Test error: {:.4f} \n'.format(test_loss, test_error))
+    print('\nTest Set, Loss: {:.4f}, Test error: {:.4f} \n'.format(test_loss, test_error))
+    # log_file.write('\nTest Set, Loss: {:.4f}, Test error: {:.4f} \n'.format(test_loss, test_error))
     file_test = open(output+'test_lost.txt', 'a')
     file_test.write('Training epoch {} \n'.format(epoch))
     file_test.write('Test Set, Loss: {:.4f}, Test error: {:.4f} \n'.format(test_loss, test_error))
